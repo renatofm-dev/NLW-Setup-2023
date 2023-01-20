820b6177ca28ad884a5b27283ebeb446ee818eeb
Renato Martins <rmartins6694@gmail.com>
2023-01-19 22:49:11 -0300

    Create new component SummaryTable.tsx but not finish

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 // import { Habit } from "./components/Habit";
 import './styles/global.css';
 import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
 
 
@@ -10,6 +11,7 @@ export function App() {
    <div className="w-screen h-screen flex justify-center items-center">
     <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
       <Header />
+      <SummaryTable />
     </div>
 
    </div>
