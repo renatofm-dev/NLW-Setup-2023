d86553e7d55564cd925374198942ff306786282c
Renato Martins <rmartins6694@gmail.com>
2023-01-22 10:36:59 -0300

    improve SummaryTable and NewHabitForm

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 // import { Habit } from "./components/Habit";
 import './styles/global.css';
+import './lib/dayjs'
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
 
