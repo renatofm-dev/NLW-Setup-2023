b30416fda733bffdfba1402e7b15155fa7c3835b
Renato Martins <rmartins6694@gmail.com>
2023-01-22 14:48:31 -0300

    remove localtunel

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,15 @@
-//Back-and API RESTful
-import Fastify from "fastify";
-import cors from "@fastify/cors";
-import { appRoutes } from "./routes";
-import localtunnel from 'localtunnel';
-const app = Fastify();
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
+const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
 
-
-
-app
-.listen({
-	host: '192.168.1.4',
-	port: 3333,
+app.listen({
+  port: 3333,
+  host: "0.0.0.0",
+}).then(() => {
+  console.log('HTTP Server running!')
 })
-.then(async () => {
-	const tunnel = await localtunnel({
-		local_host: '192.168.1.4',
-		port: 3333,
-		subdomain: 'rfmapihabits',
-	});
-
-	console.log('HTTP Server Running!');
-});
