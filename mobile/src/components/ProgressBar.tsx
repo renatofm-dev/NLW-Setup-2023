925e2a3b2ed2b43f410eb1a28ca99a80da121bb1
Renato Martins <rmartins6694@gmail.com>
2023-01-25 12:29:59 -0300

    finished features

--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
 
 interface Props {
@@ -6,12 +8,25 @@ interface Props {
 }
 
 export function ProgressBar ({ progress = 0 }: Props) {
+    const sharedProgress = useSharedValue(progress)
+
+    const style = useAnimatedStyle(()=> {
+        return {
+            width: `${sharedProgress.value}%`
+        };
+    });
+
+    useEffect(() => {
+        sharedProgress.value = withTiming(progress);
+    }, [progress])
+
     return (
         <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-        <View 
+        <Animated.View 
             className="w-full h-3 rounded-xl bg-violet-600"
-            style={{width: `${progress}%`}}
+            style={style}
         />
     </View>
     )
 }
+
